5b6b3c64536c8859a20b3736b4bdd45948dd6115
ray <ray@health263.systems>
2019-01-16 20:53:58 +0200

    added the project model and the project controller

---
+++ controllers/project.go
@@ -0,0 +1,50 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/astaxie/beego/validation"
+	"github.com/raykanavheti/LetsworkBackend/controllers/util"
+	"github.com/raykanavheti/LetsworkBackend/models"
+)
+// ProjectController interface
+type ProjectController struct{}
+
+// CreateProject creates a new project
+func (ProjCtrl *ProjectController) CreateProject(w http.ResponseWriter, r *http.Request) {
+	responseWriter := util.GetResponseWriter(w, r)
+	defer responseWriter.Close()
+	project := models.Project{}
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&project)
+
+	if err != nil {
+		mapError := map[string]string{"message": err.Error()}
+		errj, _ := json.Marshal(mapError)
+		responseWriter.WriteHeader(400)
+		responseWriter.Write(errj)
+	} else {
+		valid := validation.Validation{}
+		b, err := valid.Valid(project)
+		if !b {
+			mapError := map[string]string{"message": err.Error()}
+			errj, _ := json.Marshal(mapError)
+			responseWriter.WriteHeader(400)
+			responseWriter.Write(errj)
+		} else {
+			cat, err := models.CreateProject(project)
+			if err == nil {
+				uj, _ := json.Marshal(cat)
+				responseWriter.Header().Set("Content-Type", "application/json")
+				responseWriter.WriteHeader(201)
+				responseWriter.Write(uj)
+			} else {
+				mapError := map[string]string{"message": err.Error()}
+				errj, _ := json.Marshal(mapError)
+				responseWriter.WriteHeader(400)
+				responseWriter.Write(errj)
+			}
+		}
+	}
+}
--- models/project.go
+++ models/project.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/jinzhu/gorm"
 )
 
@@ -19,9 +21,39 @@ type Project struct {
 	Budget     struct {
 		minimum float32
 		maximum float32
-	} `sql:"budget,type:jsonb"`
+	} `sql:"type:jsonb"`
 	ProjectType string // fixed or hourly
 	Project     ProjectCategory
 	Tasks       []*Task
 	OwnerID     uint
+}
+
+// CreateProject method creates a project
+func CreateProject(userProject Project) (Project, error) {
+
+	db, err := getDBConnection()
+	defer db.Close()
+	if err == nil {
+		err := db.Save(&userProject).Error
+		if err == nil {
+			return userProject, nil
+		}
+		return userProject, errors.New("Unable to create user for session " + err.Error())
+	}
+	return userProject, errors.New("Unable to getdatabase connection")
+}
+
+// UpdateProject method updates a project
+func UpdateProject(userProject Project) (Project, error) {
+
+	db, err := getDBConnection()
+	defer db.Close()
+	if err == nil {
+		err := db.Save(&userProject).Error
+		if err == nil {
+			return userProject, nil
+		}
+		return userProject, errors.New("Unable to create user for session " + err.Error())
+	}
+	return userProject, errors.New("Unable to getdatabase connection")
 }
