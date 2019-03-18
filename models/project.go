87688bde2e4c7735f7c412c279cf013e6c895e9d
ray <ray@health263.systems>
2019-03-18 10:16:15 +0200

    git ignore file

--- models/project.go
+++ models/project.go
@@ -31,20 +31,6 @@ type Project struct {
 	OwnerID      uint
 }
 
-// Value method
-// func (j JSONB) Value() (driver.Value, error) {
-// 	valueString, err := json.Marshal(j)
-// 	return string(valueString), err
-// }
-//
-// // Scan method
-// func (j *JSONB) Scan(value interface{}) error {
-// 	if err := json.Unmarshal(value.([]byte), &j); err != nil {
-// 		return err
-// 	}
-// 	return nil
-// }
-
 // CreateProject method creates a new project
 func CreateProject(userProject Project) (Project, error) {
 
